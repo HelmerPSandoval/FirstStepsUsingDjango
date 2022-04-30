9a693ceafb0b8d5c8510f38b53f53ef37beec35c
HelmerPSandoval <alonsospider_1998@hotmail.com>
2022-04-29 20:16:32 -0400

    Creación de superuser y de vistas 'HelloWorld' y 'PetListAPIView' dentro del proyecto

--- personalProject/personalProject/urls.py
+++ personalProject/personalProject/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
+
 from django.contrib import admin
 from django.urls import path, include
+from petclub.views import PetListAPIView
+from petclub.views import (
+    HelloWorld,
+    PetListAPIView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('hi', HelloWorld.as_view(), name="helloworld"),
+    path('pets/', PetListAPIView.as_view(), name="all-pets"),
 ]
--- personalProject/petclub/views.py
+++ personalProject/petclub/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import (
+    RetrieveAPIView,
+    ListAPIView,
+)
 
 # Create your views here.
+class HelloWorld(APIView):
+
+    def get(self,request):
+        return Response(data="Este es el método get!", status=200)
+    def patch(self,request):
+        return Response(data="Este es el método patch!", status=200)
+
+    def delete(self,request):
+        return Response(data="Este es el método delete!", status=200)
+
+    def post(self,request):
+        return Response(data="Este es el método post!", status=200)
+
+class PetListAPIView(ListAPIView):
+
+    def get(self,request):
+        return Response(data="Estas son todas mis mascotas!", status=200)
+
+class PetAPIView(RetrieveAPIView):
+
+    def get(self,request):
+        return Response(data="Este es el método get de PetAPIView!", status=200)
